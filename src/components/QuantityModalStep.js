49d8af7dcea224dc03e53d46e9a352116d38807f
echo <hello@kevinjamesneal.us>
2022-09-08 12:40:53 -0400

    2

---
+++ src/components/QuantityModalStep.js
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Slider } from '@mui/material';
+import {
+    getDefaultMaxTokensPerMint,
+    getMaxSupply,
+    getMaxTokensPerMint,
+    getMintedNumber,
+    getMintPrice,
+    mint
+} from '../mint/web3';
+import { showAlert } from './AutoHideAlert';
+import { parseTxError, roundToDecimal } from '../utils';
+import { Attribution } from './Attribution';
+import { isEthereumContract } from "../contract";
+
+export const QuantityModalStep = ({ setQuantity, setIsLoading, setTxHash, setStep }) => {
+    const [quantityValue, setQuantityValue] = useState(1)
+    const [maxTokens, setMaxTokens] = useState(getDefaultMaxTokensPerMint())
+    const [mintPrice, setMintPrice] = useState(undefined)
+    const [mintedNumber, setMintedNumber] = useState()
+    const [totalNumber, setTotalNumber] = useState()
+
+    useEffect(() => {
+        if (isEthereumContract()) {
+            getMintPrice().then(price => {
+                if (price !== undefined) {
+                    setMintPrice(Number((price) / 1e18))
+                }
+            })
+        }
+        getMaxTokensPerMint().then(setMaxTokens)
+        if (!window.DEFAULTS?.hideCounter) {
+            getMintedNumber().then(setMintedNumber)
+            getMaxSupply().then(setTotalNumber)
+        }
+    }, [])
+
+    const maxTokensTooLarge = maxTokens >= 20
+    const step = !maxTokensTooLarge ? maxTokens : 10
+    const marks = [
+        ...[...Array(Math.floor(maxTokens / step) + 1)].map((_, i) => 1 + i * step),
+        ...(maxTokensTooLarge ? [maxTokens + 1] : [])
+        ].map(m => ({
+            value: Math.max(1, m - 1),
+            label: (Math.max(1, m - 1)).toString()
+        }))
+
+    const onSuccess = async () => {
+        setIsLoading(true)
+        const { tx } = await mint(quantityValue)
+        if (tx === undefined) {
+            setIsLoading(false)
+        }
+        tx?.on("transactionHash", (hash) => {
+            setTxHash(hash)
+        })?.on("confirmation", async () => {
+            setIsLoading(false)
+            showAlert(`Successfully minted ${quantityValue} NFTs`, "success")
+        })?.on("error", (e) => {
+            setIsLoading(false)
+            const { code, message } = parseTxError(e);
+            if (code !== 4001) {
+                showAlert(`Minting error: ${message}. Please try again or contact us`, "error");
+            }
+        })
+    }
+
+    return <div style={{ width: "100%" }}>
+        {maxTokens > 1 && <Box sx={{
+            display: "flex",
+            alignItems: "flex-end",
+            width: "100%",
+            height: 64
+        }}>
+            <Slider
+                sx={{ ml: 2 }}
+                aria-label="Quantity"
+                defaultValue={1}
+                valueLabelDisplay="auto"
+                onChange={(e, v) => {
+                    setQuantity(v)
+                    setQuantityValue(v)
+                }}
+                step={1}
+                marks={marks}
+                min={1}
+                max={maxTokens}
+            />
+        </Box>}
+        <Button
+            onClick={onSuccess}
+            sx={{ mt: maxTokens > 1 ? 4 : 2, width: "100%" }}
+            variant="contained"
+        >
+            {mintPrice !== undefined
+                ? (mintPrice !== 0 ? `Mint for ${roundToDecimal(mintPrice * quantityValue, 4)} ETH` : "Mint for free")
+                : "Mint"}
+        </Button>
+        {!window.DEFAULTS?.hideCounter && <Box
+            sx={{
+                color: (theme) => theme.palette.grey[500],
+                display: "flex",
+                fontWeight: 400,
+                fontSize: 14,
+                justifyContent: "center",
+                mt: 2
+            }}>
+            <span>{mintedNumber ?? "-"}</span>
+            <span style={{ margin: "0 2px"}}>/</span>
+            <span>{totalNumber ?? "-"}</span>
+        </Box>}
+    </div>
+}
---
+++ src/contract.js
@@ -0,0 +1,130 @@
+import { getCurrentNetwork, isWeb3Initialized, switchNetwork, web3 } from './wallet.js';
+import { normalizeURL } from "./utils.js";
+import { NETWORKS } from "./constants.js";
+
+export let NFTContract;
+
+const abiMemoryCache = {};
+
+export const initContract = async (contract, shouldSwitchNetwork=true) => {
+    const host = normalizeURL(window.location.href);
+    const allowedURLs = contract?.allowedURLs?.map(u => normalizeURL(u));
+    if (allowedURLs && !allowedURLs?.some(v => v.includes(host))) {
+        return undefined;
+    }
+    let currentNetwork = await getCurrentNetwork();
+    if (shouldSwitchNetwork && !contract.allowedNetworks.includes(currentNetwork)) {
+        await switchNetwork(contract.allowedNetworks[0])
+        currentNetwork = await getCurrentNetwork();
+    }
+    const address = contract.address[contract.allowedNetworks[0]];
+    const abi = contract.abi;
+    return new web3.eth.Contract(abi, address);
+}
+
+const initContractGlobalObject = async () => {
+    if (window.CONTRACT_ADDRESS === "YOUR CONTRACT ADDRESS HERE") {
+        alert("You forgot to insert your NFT contract address in your Webflow Embed code. Insert your contract address, publish the website and try again. If you don't have it, contact https://buildship.xyz")
+        return
+    }
+    const chainID = getConfigChainID()
+    const implementationAddress = window.IMPLEMENTATION_ADDRESS ?? window.CONTRACT_ADDRESS
+    window.CONTRACT = {
+        nft: {
+            address: {
+                [chainID]: window.CONTRACT_ADDRESS,
+            },
+            abi: await fetchABI(implementationAddress, chainID),
+            allowedNetworks: [chainID]
+        }
+    }
+}
+
+export const getConfigChainID = () => {
+    // Default to Ethereum
+    const networkID = window.NETWORK_ID ?? 1;
+    return window.IS_TESTNET ? NETWORKS[networkID].testnetID : networkID;
+}
+
+export const fetchABI = async (address, chainID) => {
+    if (abiMemoryCache[address])
+        return abiMemoryCache[address]
+
+    const remoteABI = await fetchRemoteCachedABI(address)
+    console.log("REMOTE CACHED ABI", remoteABI)
+    if (remoteABI)
+        return remoteABI
+
+    const abi = await fetch(`https://metadata.buildship.xyz/api/v1.1/contract/${address}?network_id=${chainID}`)
+        .then(r => r.json())
+        .then(async ({ abi, isProxy, implementation }) => {
+            if (isProxy) {
+                if (implementation) {
+                    return await fetchABI(implementation, chainID)
+                } else {
+                    console.error("Couldn't fetch ABI for proxy with undefined implementation address ")
+                    return null
+                }
+            }
+            return abi
+        })
+        .catch(e => null)
+
+    if (!abi) {
+        console.log("No ABI returned from https://metadata.buildship.xyz")
+        const embeddedMainABI = getEmbeddedMainABI(address)
+        if (!embeddedMainABI) {
+            alert(`Error: no ABI loaded for ${address}. Please contact support`)
+        }
+        return embeddedMainABI;
+    }
+    abiMemoryCache[address] = abi;
+    return abi;
+}
+
+const fetchRemoteCachedABI = async (address) => {
+    if (!window.DEFAULTS?.abiCacheURL) {
+        return null
+    }
+
+    console.log("Trying to load ABI from cache URL", address)
+    try {
+        return await fetch(window.DEFAULTS?.abiCacheURL)
+            .then(r => r.json())
+            .then(r => Object.keys(r).reduce((acc, key) => {
+                acc[key.toLowerCase()] = r[key]
+                return acc
+            }, {}))
+            .then(r => r[address.toLowerCase()])
+    } catch (e) {
+        alert("Wrong format for ABI cache. Should be a URL to .json file. Fix or remove ABI cache URL to resolve")
+        console.log("Wrong format for ABI cache", e)
+    }
+}
+
+const getEmbeddedMainABI = (address) => {
+    if (address?.toLowerCase() === window.CONTRACT_ADDRESS?.toLowerCase()) {
+        console.log("Trying to load embedded main contract ABI")
+        return typeof window.CONTRACT_ABI === 'string'
+            ? JSON.parse(window.CONTRACT_ABI)
+            : window.CONTRACT_ABI
+    }
+    return undefined
+}
+
+export const setContracts = async (shouldSwitchNetwork=true) => {
+    await initContractGlobalObject();
+    if (!isWeb3Initialized()) {
+        return
+    }
+    if (shouldSwitchNetwork) {
+        await switchNetwork(window.CONTRACT.nft.allowedNetworks[0]);
+    }
+    if (NFTContract) {
+        return
+    }
+    NFTContract = await initContract(window.CONTRACT.nft, false);
+    console.log("NFTContract", NFTContract)
+}
+
+export const isEthereumContract = () => ([1, 4].includes(window.CONTRACT.nft.allowedNetworks[0]))
